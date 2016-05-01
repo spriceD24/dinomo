cf2e144cd59a36e7e0469f5b1d5b3bc120fedf3d
spriceD24 <sprice_D24@yahoo.com>
2016-05-01 22:13:09 +1000

    Added isProd
    Added isProd

--- util/WebUtil.php
+++ util/WebUtil.php
@@ -7,5 +7,19 @@
 			$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 			return substr($url, 0, strrpos( $url, '/'));
 		}
+		
+		function isProduction()
+		{
+			$host = $_SERVER["HTTP_HOST"];
+			$pos = strpos($host,"localhost");
+			
+			if ($pos !== false) 
+			{
+				return 0;	
+			}
+			return 1;
+		}
+		
+		
 	}	
 ?>
