73e7c006f524974c994e9ece2af581ea92d9ad09
spriceD24 <sprice_D24@yahoo.com>
2016-05-19 06:33:17 +1000

    debugging
    debugging

--- showCookies.php
+++ showCookies.php
@@ -10,9 +10,7 @@ $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
 $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
 
 $stringUtil = new StringUtils();
-$str = $stringUtil->encode("pnoonan");
-echo "Encoded = ".$str."<br/>";
-echo "Decoded = ".$stringUtil->decode($str)."<br/>";
+
 
 $cookie_name = WebUtil::DINAMO_USER;
 
@@ -22,6 +20,9 @@ if(!isset($_COOKIE[$cookie_name])) {
     echo "Cookie '" . $cookie_name . "' is set!<br>";
     echo "Value is: " . $_COOKIE[$cookie_name];
 }
+
+echo "<br/><br/>";
+print_r($_SERVER);
 ?>
 
 </body>
