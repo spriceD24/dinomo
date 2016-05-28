45e0d57f385ab27a7945d1c623102ba8c7a8fb74
spriceD24 <sprice_D24@yahoo.com>
2016-05-28 22:57:02 +1000

    Added Terry
    Added Terry

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -8,7 +8,8 @@ class UserDAO {
 		
 		$users->add ( new User ( 1, 'Stephen Price', 'sprice', 'stefdogd24@gmail.com', '343433440450', '','recipient' ) );
 		$users->add ( new User ( 2, 'Patrick Noonan', 'pnoonan', 'patricknoonan@dinomoformwork.com.au', '0303040450', '','user' ) );
-		$users->add ( new User ( 3, 'John Smith', 'jsmith', 'john@johnsite.com', '3434343','', 'user' ) );
+		$users->add ( new User ( 3, 'Terence McGarrigle', 'terry', 'terrymc_garrigle@hotmail.com', '0303040450', '','user' ) );
+		$users->add ( new User ( 4, 'John Smith', 'jsmith', 'john@johnsite.com', '3434343','', 'user' ) );
 		
 		return $users;
 	}
