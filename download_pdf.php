6ff8c254c698bb0d3feccb61b4023aebeed106b5
spriceD24 <sprice_D24@yahoo.com>
2016-05-30 22:35:30 +1000

    Fixed
    Fixed

--- download_pdf.php
+++ download_pdf.php
@@ -1,7 +1,7 @@
 <?php include_once("util/ConfigUtil.php"); ?><html>
 <body>
 
-<?php	if(isset($_GET ["report"]))	{		$fileKey = urldecode($_GET ["report"]);		$pdf_folder = ConfigUtil::getPDFFolder ();		$path = realpath ( '.' );				$file = $path . '/' . $pdf_folder . '/' . $fileKey . '.pdf';				if (file_exists($file)) {		    header('Content-Description: File Transfer');		    header('Content-Type: application/octet-stream');		    header('Content-Disposition: attachment; filename="'.basename($file).'"');		    header('Expires: 0');		    header('Cache-Control: must-revalidate');		    header('Pragma: public');		    header('Content-Length: ' . filesize($file));		    readfile($file);		    exit;		}	}			?>
+<?php	if(isset($_GET ["report"]))	{		$fileKey = urldecode($_GET ["report"]);		$pdf_folder = ConfigUtil::getPDFFolder ();		$path = realpath ( '.' );				$file = $path . '/' . $pdf_folder . '/' . $fileKey . '.pdf';				if (file_exists($file)) {		    header('Content-Description: File Transfer');		    header('Content-Type: application/octet-stream');		    header('Content-Disposition: attachment; filename="'.basename($file).'"');		    header('Expires: 0');		    header('Cache-Control: must-revalidate');		    header('Pragma: public');		    header('Content-Length: ' . filesize($file));		    readfile($file);		    exit;		}else{			echo "<h1>File Not Found</h1>";		}	}			?>
 
 </body>
 </html>
